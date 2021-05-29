807cea143d36453b0247a4ba6d00f80f5d5ecc88
Wadson Vaval <vavalson@hotmail.com>
2021-05-28 20:20:05 -0400

    Creating Store page

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import Routes from './app/navigation'
-
+import MainList from './app/screens/MainList'
+import StoreScreen from './app/screens/StoreScreen'
 export default function App() {
-  return <Routes />
+  return <StoreScreen />
 }
